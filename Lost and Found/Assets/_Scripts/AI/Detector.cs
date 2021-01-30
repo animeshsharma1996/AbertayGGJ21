a58b09cdc8f86791129b31707dcffcf7870c4f36
Animesh Sharma <animeshsharma1996@gmail.com>
2021-01-30 19:27:09 +0000

    Detection System

---
+++ Lost and Found/Assets/_Scripts/AI/Detector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detector : MonoBehaviour
+{
+    [SerializeField] private float range = 0f;
+    [SerializeField] private float fovAngle = 0f;
+    [SerializeField] private GameObject[] players = null;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        foreach(GameObject player in players)
+        {
+            Vector3 vectorToPlayer = player.transform.position - transform.position;
+            float angle = Vector3.Angle(vectorToPlayer, transform.forward);
+
+            if(angle <= fovAngle*0.5f)
+            {
+                RaycastHit hit;
+                if(Physics.Raycast(transform.position, vectorToPlayer.normalized, out hit, range))
+                {
+                    if(hit.collider.gameObject.tag == "Player")
+                    {
+                        Debug.Log("Detected");
+                    }
+                }
+            }
+        }
+    }
+}
