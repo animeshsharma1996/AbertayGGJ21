d9cd3b74bd391dd6de7cfb39397f5e106b52c39d
mu5hjj <james.mushahwar@gmail.com>
2021-01-30 01:37:12 +0000

    Finished cross platform movement for Lost and Found

---
+++ Lost and Found/Assets/_Scripts/Player/CharacterMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private float currentSpeed;
+    private float speedSmoothVelocity;
+    [SerializeField]
+    private float speedSmoothTime;
+    [SerializeField]
+    private float maxMovementSpeed;
+
+    private CharacterController controller;
+
+    [SerializeField]
+    private List<string> acceptedKeyCodes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        MoveUpdate();
+    }
+
+    void MoveUpdate()
+    {
+        Vector3 vInput = new Vector3(CrossPlatformInputManager.GetAxis(acceptedKeyCodes[0]), 0f, CrossPlatformInputManager.GetAxis(acceptedKeyCodes[1]));
+        Vector3 vInputDir = vInput.normalized;
+
+        float vTargetSpeed = maxMovementSpeed * vInputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, vTargetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        controller.Move(vInputDir * currentSpeed * Time.deltaTime);
+    }
+}
