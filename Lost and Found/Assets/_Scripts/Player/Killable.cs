eb019324681dd57dd0e16b8795ba388255cc810c
mu5hjj <james.mushahwar@gmail.com>
2021-01-31 10:42:35 +0000

    Added sounds and converted prefabs to 2d

--- Lost and Found/Assets/_Scripts/Player/Killable.cs
+++ Lost and Found/Assets/_Scripts/Player/Killable.cs
@@ -11,7 +11,7 @@ public class Killable : MonoBehaviour
     private int currentLives = 3;
     [SerializeField]
     private float cooldownTimer = 4.0f;
-    private Material sr;
+    private SpriteRenderer sr;
 
     public bool IsAlive { get => isAlive; set => isAlive = value; }
     public bool IsCoolingDown { get => isCoolingDown; set => isCoolingDown = value; }
@@ -25,7 +25,7 @@ public class Killable : MonoBehaviour
         flash = DamageFlash();
         currentLives = maxLives;
         isAlive = true;
-        sr = GetComponent<MeshRenderer>().material;
+        sr = GetComponent<SpriteRenderer>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -74,6 +74,7 @@ public class Killable : MonoBehaviour
             yield return 0;
         }
         StopCoroutine(flash);
+        sr.color = Color.white;
         IsCoolingDown = false;
     }
 
